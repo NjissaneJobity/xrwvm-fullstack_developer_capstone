0477da55752cb108acbd1472c9ab503e9e3b3bf3
NjissaneJobity <145627945+NjissaneJobity@users.noreply.github.com>
2025-03-14 16:14:40 -0400

    Update admin.py

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -4,17 +4,17 @@ from .models import CarMake, CarModel
 
 # Admin class for CarMake
 class CarMakeAdmin(admin.ModelAdmin):
-    list_display= ('name', 'country', 'description')  
-    search_fields= ('name', 'country')  
-    list_filter= ('country',) 
+    list_display = ('name', 'country', 'description')
+    search_fields = ('name', 'country')
+    list_filter = ('country',)
 
 
 # Admin class for CarModel
 class CarModelAdmin(admin.ModelAdmin):
-    list_display= ('name', 'car_make', 'type', 'year', 'fuel_type')
-    search_fields= ('name', 'car_make__name') 
-    list_filter= ('type', 'year', 'fuel_type')  
-    list_select_related= ('car_make',) 
+    list_display = ('name', 'car_make', 'type', 'year', 'fuel_type')
+    search_fields = ('name', 'car_make__name')
+    list_filter = ('type', 'year', 'fuel_type')
+    list_select_related = ('car_make',)
 
 
 # Register models with admin classes
