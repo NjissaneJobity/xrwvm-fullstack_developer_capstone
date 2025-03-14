47e89ef1e33575191c6c7e81c1754d9dff5e1b8d
NjissaneJobity <145627945+NjissaneJobity@users.noreply.github.com>
2025-03-14 17:45:38 -0400

    Update settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -30,11 +30,17 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     'localhost',
-    'https://njissanejobi-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai',
+    (
+        'https://njissanejobi-8000.theiadockernext-0-labs-'
+        'prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
+    ),
 ]
 
 CSRF_TRUSTED_ORIGINS = [
-    'https://njissanejobi-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai',
+    (
+        'https://njissanejobi-8000.theiadockernext-0-labs-'
+        'prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
+    ),
 ]
 
 REST_FRAMEWORK = {
